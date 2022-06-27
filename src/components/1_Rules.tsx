3cd6c1c5a174d5e02205e8e54abcfc823f524aa3
etokruto <akrut26@gmail.com>
2022-06-27 15:12:03 -0700

    clean up

--- src/components/1_Rules.tsx
+++ src/components/1_Rules.tsx
@@ -26,138 +26,130 @@ const Rules: React.FC<RuleProps> = ({ onSubmitPage1 }: RuleProps) => {
   function onSubmit(data: any, e: any) {
     e.preventDefault();
     onSubmitPage1(data);
-
   }
 
   return (
-    <div className="section">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <h1>Ground Rules</h1>
-        <div
-          className="main-container"
-          style={{ display: 'flex', justifyContent: 'space-between' }}
-        >
-          <div className="left-container">
-            <p>
-              3 Rounds <br />
-              2 Indecisive People <br />
-              1 Choice - Where to Eat <br /> <br />
-              <b style={{ color: 'rose' }}>Please fill out this page: </b>
-            </p>
-            <h3>Person 1: </h3>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Ground Rules</h1>
+      <div className="main-container">
+        <div className="left-container" style={{ flexDirection: 'row' }}>
+          3
+          <br />
+          Rounds <br />
+          <br />
+          2
+          <br /> Indecisive People <br />
+          <br />
+          1
+          <br /> Choice - Where to Eat <br /> <br />
+        </div>
+        <div className="right-container">
+          <b>Please fill out this page: </b>
+
+          <h4>Person 1: </h4>
+          <input
+            className="inputField"
+            {...register('person1')}
+            type="text"
+            name="person1"
+            placeholder="First Name Please"
+            id="person1"
+            required
+          />
+          <h4>Person 2: </h4>
+          <input
+            className="inputField"
+            {...register('person2')}
+            type="text"
+            name="person2"
+            placeholder="First Name Please"
+            id="person2"
+            required
+          />
+          <div>
+            Let's choose a Zip Code:
+            <br />
             <input
-              {...register('person1')}
-              type="text"
-              name="person1"
-              placeholder="First Name Please"
-              id="person1"
+              className="inputField"
+              {...register('zipcode', { maxLength: 5 })}
+              type="number"
+              name="zipcode"
+              placeholder="5 digits please"
+              minLength={5}
+              id="zipcode"
               required
             />
-            <h3>Person 2: </h3>
+            How far do you wanna drive?:
+            <br />
             <input
-              {...register('person2')}
-              type="text"
-              name="person2"
-              placeholder="First Name Please"
-              id="person2"
+              className="inputField"
+              {...register('radius', { maxLength: 2 })}
+              type="number"
+              name="radius"
+              placeholder="in Miles (you're in US right?)"
+              minLength={1}
+              id="radius"
               required
             />
           </div>
-          <div className="right-container">
-            <h3>ZipCode</h3>
+          <div>
             <div>
-              Let's choose a Zip Code:
-              <br />
-              <input
-                {...register('zipcode', { maxLength: 5 })}
-                type="number"
-                name="zipcode"
-                placeholder="5 digits please"
-                minLength={5}
-                id="zipcode"
-                required
-              />
-              <br />
-              <br />
-              How far do you wanna drive?:
-              <br />
-              <input
-                {...register('radius', { maxLength: 2 })}
-                type="number"
-                name="radius"
-                placeholder="in Miles (you're in US right?)"
-                minLength={1}
-                id="radius"
-                required
-              />
-            </div>
-            <div>
-              <div>
-                Can we agree on the price?
-                <div className="radio-buttons">
-                  <label>
-                    <input
-                      {...register('price')}
-                      type="radio"
-                      name="price"
-                      value="$$$"
-                      required
-                    />
-                    $$$
-                  </label>
-                  <label>
-                    <input
-                      {...register('price')}
-                      type="radio"
-                      name="price"
-                      value="$$"
-                      required
-                    />
-                    $$
-                  </label>
-                  <label>
-                    <input
-                      {...register('price')}
-                      type="radio"
-                      name="price"
-                      value="$"
-                      required
-                    />
-                    $$
-                  </label>
-                </div>
+              Can we agree on the price?
+              <div className="radio-buttons">
+                <input
+                  className="inputField"
+                  {...register('price')}
+                  type="radio"
+                  name="price"
+                  value="$$$"
+                  required
+                />
+                $$$
+                <input
+                  className="inputField"
+                  {...register('price')}
+                  type="radio"
+                  name="price"
+                  value="$$"
+                  required
+                />
+                $$
+                <input
+                  className="inputField"
+                  {...register('price')}
+                  type="radio"
+                  name="price"
+                  value="$"
+                  required
+                />
+                $$
               </div>
             </div>
-            <br />
-            <div>
-              Do we want to try something new?
-              <label>
-                <input
-                  {...register('new')}
-                  type="checkbox"
-                  name="new"
-                  value="true"
-                />
-              </label>
-            </div>
+          </div>
+          <br />
+          <div className="new-input">
+            Do we want to try something new?
+            <input
+              className="inputField"
+              {...register('new')}
+              type="checkbox"
+              name="new"
+              value="true"
+            />
           </div>
         </div>
-        <h4 style={{ padding: 20 }}>
-          {watch('person1') || 'Person 1'} and {watch('person2') || 'Person 2'}{' '}
-          have no idea what they want to eat, but they do know they want
-          something at most {watch('radius') || 'something close by.'}{' '}
-          {watch('radius') ? 'miles away.' : ''} Let's start by pressing the
-          button below
-        </h4>
-        <button
-          style={{ height: 60, minWidth: '20%' }}
-          type="submit"
-
-        >
-          Send
-        </button>
-      </form>
-    </div>
+      </div>
+      <h4 style={{ padding: 20 }}>
+        {watch('person1') || 'Person 1'} and {watch('person2') || 'Person 2'}{' '}
+        have no idea what they want to eat, but they do know they want something
+        at most {watch('radius') || 'something close by.'}{' '}
+        {watch('radius') ? 'miles away.' : ''} Let's start by pressing the
+        button below
+      </h4>
+      <button style={{ height: 60, minWidth: '20%' }} type="submit">
+        Move On
+      </button>
+    </form>
   );
 };
 
--- src/components/2_Mood.tsx
+++ src/components/2_Mood.tsx
@@ -21,6 +21,8 @@ const Mood: React.FC<MoodProps> = ({
   selectionTags,
   players,
 }: MoodProps) => {
+  let placeholderTagText = 'Choose a tag from the right or add your own';
+  let placeholderTagStyle = { minHeight: '5vh', margin: 20, minWidth: '40vh' };
   const {
     register,
     handleSubmit,
@@ -31,113 +33,141 @@ const Mood: React.FC<MoodProps> = ({
   function onSubmit(data: any, e: any) {
     e.preventDefault();
     onSubmitPage2(data);
-
   }
 
   return (
-    <div className="section">
-      <h1 style={{ padding: 20 }}>
-        {players[0]} - What are you in the Mood for?{' '}
-      </h1>
-      <h2> </h2>
-      <div
-        className="main-container"
-        style={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        <div className="left-container">
-          Input Tags here, up to 5 :
-          <form
-            style={{ display: 'flex', flexDirection: 'column' }}
-            onSubmit={handleSubmit(onSubmit)}
-          >
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="section">
+        <h2 style={{ padding: 20 }}>
+          <span style={{ fontSize: '5vh' }}> {players[0] || 'Person 1'} </span>{' '}
+          what are you in the Mood for?{' '}
+        </h2>
+        <div className="main-container">
+          <div className="left-container">
+            Input Tags here, up to 5 :
             <input
+              className="inputField"
               {...register('tags_1')}
               type="text"
               name="tags_1"
-              placeholder="Choose from the right or add your own"
+              placeholder={placeholderTagText}
               id="tags_1"
-              style={{ minHeight: '5vh', margin: 20, minWidth: '40vh' }}
+              style={placeholderTagStyle}
             />
             <input
+              className="inputField"
               {...register('tags_2')}
               type="text"
               name="tags_2"
-              placeholder="Choose from the right or add your own"
+              placeholder={placeholderTagText}
               id="tags_2"
-              style={{ minHeight: '5vh', margin: 20, minWidth: '40vh' }}
+              style={placeholderTagStyle}
             />
             <input
+              className="inputField"
               {...register('tags_3')}
               type="text"
               name="tags_3"
-              placeholder="Choose from the right or add your own"
+              placeholder={placeholderTagText}
               id="tags_3"
-              style={{ minHeight: '5vh', margin: 20, minWidth: '40vh' }}
+              style={placeholderTagStyle}
             />
             <input
+              className="inputField"
               {...register('tags_4')}
               type="text"
               name="tags_4"
-              placeholder="Choose from the right or add your own"
+              placeholder={placeholderTagText}
               id="tags_4"
-              style={{ minHeight: '5vh', margin: 20, minWidth: '40vh' }}
+              style={placeholderTagStyle}
             />
             <input
+              className="inputField"
               {...register('tags_5')}
               type="text"
               name="tags_5"
-              placeholder="Choose from the right or add your own"
+              placeholder={placeholderTagText}
               id="tags_5"
-              style={{ minHeight: '5vh', margin: 20, minWidth: '40vh' }}
+              style={placeholderTagStyle}
             />
-
-            <button
-              style={{ height: 80, minWidth: '20%' }}
-              type="submit"
-
-            >
-              Submit after you've entered Tags
-            </button>
-          </form>
-        </div>
-        <div className="right-container" style={{ minWidth: 400 }}>
-          <h3>Selection of Tags Available</h3>
-          <div className="marquee-container-outline">
-            {selectionTags.length ? (
-              <Marquee
-                marqueeItems={selectionTags}
-                direction={'up'}
-                height={400}
-                delay={30}
-                key={selectionTags[0]}
-              />
-            ) : (
-              <Marquee
-                marqueeItems={tags}
-                direction={'up'}
-                height={400}
-                delay={30}
-                key={selectionTags[0]}
-              />
-            )}
+          </div>
+          <div className="right-container" style={{ minWidth: 400 }}>
+            <h3>Selection of Tags Available</h3>
+            <table style={{ width: '100%' }}>
+              <tbody>
+                <tr>
+                  {' '}
+                  <td>
+                    <div className="marquee-container-outline">
+                      {selectionTags.length ? (
+                        <Marquee
+                          marqueeItems={selectionTags}
+                          direction={'up'}
+                          minHeight={400}
+                          delay={30}
+                          key={selectionTags[0]}
+                        />
+                      ) : (
+                        <Marquee
+                          marqueeItems={tags}
+                          direction={'up'}
+                          minHeight={400}
+                          delay={30}
+                          key={selectionTags[0]}
+                        />
+                      )}
+                    </div>
+                  </td>
+                  <td>
+                    <div className="marquee-container-outline">
+                      {selectionTags.length ? (
+                        <Marquee
+                          marqueeItems={selectionTags}
+                          direction={'up'}
+                          minHeight={400}
+                          delay={20}
+                          key={selectionTags[0]}
+                        />
+                      ) : (
+                        <Marquee
+                          marqueeItems={tags}
+                          direction={'up'}
+                          minHeight={400}
+                          delay={20}
+                          key={selectionTags[0]}
+                        />
+                      )}
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div>
+              <h2 style={{ padding: 20 }}>Tags Chosen: </h2>
+              <p>
+                {watch('tags_1')}
+                {watch('tags_1') ? ', ' : ' '}
+                {watch('tags_2')}
+                {watch('tags_2') ? ', ' : ' '}
+                {watch('tags_3')}
+                {watch('tags_3') ? ', ' : ' '}
+                {watch('tags_4')}
+                {watch('tags_4') ? ', ' : ' '}
+                {watch('tags_5')}
+              </p>
+            </div>
           </div>
         </div>
+        Press{' '}
+        <button
+          style={{ height: 80, minWidth: '10%', borderRadius: 20 }}
+          type="submit"
+        >
+          Submit{' '}
+        </button>{' '}
+        after you've entered Tags
       </div>
-      <div>
-        <h2 style={{ padding: 20 }}>Tags Chosen: </h2>
-        <p>
-          {watch('tags_1')}
-          {watch('tags_1') ? ', ' : ' '}
-          {watch('tags_2')}
-          {watch('tags_2') ? ', ' : ' '}
-          {watch('tags_3')}
-          {watch('tags_3') ? ', ' : ' '}
-          {watch('tags_4')}
-          {watch('tags_4') ? ', ' : ' '}
-          {watch('tags_5')}
-        </p>
-      </div>
-    </div>
+    </form>
   );
 };
 
